8f8778255fc23f5b58ae00784e6c2f3506833c02
Brewalan <sbpk.pro@gmail.com>
2022-03-04 14:09:31 +0100

    Feat create views folder

---
+++ public/content/themes/wp-sbpk/views/projects/projects.tpl.php
@@ -0,0 +1,140 @@
+    <!-- Basic -->
+    <div class="ex-basic-1 pt-5 pb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-fluid mt-5 mb-3" src="<?= get_theme_file_uri('assets/images/project-details-large.jpg') ?>" alt="alternative">
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of ex-basic-1 -->
+    <!-- end of basic -->
+
+
+    <!-- Basic -->
+    <div class="ex-basic-1 pt-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-10 offset-xl-1">
+                    <p>Are you looking for ways to grow the user base for your mobile application? Then you have arrived at the right place. Here you will find a curated collection of landing page HTML templates that will help you build an engaging online presentation for your mobile app and convince visitors to become loyal paying users.</p>
+                    <p class="mb-5">All templates in the roundup are premium which means you need to pay for them but we're talking small amounts of money which won't break your bank account but will help authors make a living. In return you get a high quality, updated item together with high quality and very prompt technical support.</p>
+
+                    <h2 class="mb-3">Advantages of working with this template</h2>
+                    <p>Besides buying the template you need some basic web skills in order to customize it. Nothing too fancy HTML/CSS will do just fine and a little bit of image editing. You can always hire a web designer to help with the customization work while you provide the template and the content that is the most important.</p>
+                    <p class="mb-4">Riga is a landing page HTML template made with Bootstrap to help you showcase your mobile app online and persuade visitors to download it from the app stores. The author used Bootstrap to build the template and integrated a nice animated navigation that slides from outside the screen.</p>
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of ex-basic-1 -->
+    <!-- end of basic -->
+
+
+    <!-- Cards -->
+    <div class="ex-cards-1 pt-3 pb-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <!-- Card -->
+                    <div class="card">
+                        <ul class="list-unstyled">
+                            <li class="media">
+                                <span class="fa-stack">
+                                    <span class="fas fa-circle fa-stack-2x"></span>
+                                    <span class="fa-stack-1x">1</span>
+                                </span>
+                                <div class="media-body">
+                                    <h5>High Quality Service</h5>
+                                    <p>Features include an eye catching morphtext in the header, details lightbox for more details information</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div> <!-- end of card -->
+                    <!-- end of card -->
+
+                    <!-- Card -->
+                    <div class="card">
+                        <ul class="list-unstyled">
+                            <li class="media">
+                                <span class="fa-stack">
+                                    <span class="fas fa-circle fa-stack-2x"></span>
+                                    <span class="fa-stack-1x">2</span>
+                                </span>
+                                <div class="media-body">
+                                    <h5>Prompt Timely Delivery</h5>
+                                    <p>Statistics numbers for important values, card slider for testimonials, image slider for customer logos</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div> <!-- end of card -->
+                    <!-- end of card -->
+
+                    <!-- Card -->
+                    <div class="card">
+                        <ul class="list-unstyled">
+                            <li class="media">
+                                <span class="fa-stack">
+                                    <span class="fas fa-circle fa-stack-2x"></span>
+                                    <span class="fa-stack-1x">3</span>
+                                </span>
+                                <div class="media-body">
+                                    <h5>Skilled Team Involved</h5>
+                                    <p>Some useful extra pages are bundled with the template lik article details, terms conditions and privacy policy</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div> <!-- end of card -->
+                    <!-- end of card -->
+
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of ex-cards-1 -->
+    <!-- end of cards -->
+
+
+    <!-- Basic -->
+    <div class="ex-basic-1 pt-3 pb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-10 offset-xl-1">
+                    <p class="mb-5">Features include an eye catching morphtext in the header, details lightbox for more details information, statistics numbers for important values, card slider for testimonials, image slider for customer logos and working forms that will enable your landing page to provide leads in order to achieve your marketing goals.</p>
+
+                    <h2 class="mb-4">New elements added to the package</h2>
+                    <img class="img-fluid mb-5" src="images/project-details-small.jpg" alt="alternative">
+                    <p>Some useful extra pages are bundled with the template lik article details, terms conditions and privacy policy which can be customized as per your requirements. Riga has an impactful dark style design combined with warm colors and angular background shapes with the unique goal of making your mobile app stand out.</p>
+                    <p class="mb-5">Leon is an easy to customize landing page HTML template built with Bootstrap for promoting mobile apps to the online audience and making visitors download them from app stores. This template is built with HTML, CSS and Bootstrap to ensure the highest flexibility for all users.</p>
+                    <div class="text-box mb-5">
+                        <h3>Visitors love a beautiful and efficient website</h3>
+                        <p>All designers, developers and tech-savvy people will be able to customize this template with basic web coding skills. Among the features you will find details lightbox for more details information, tabbed content for feature details, video lightbox, card slider for testimonials, statistics numbers, image slider for customer logos, dropdown navigation and useful extra pages for article details, terms.</p>
+                    </div> <!-- end of text-box -->
+                    <p class="mb-4">Together with touches of call to action green, the overall landing page design will showcase your app beautifully and will help with convincing visitors to download it from the app stores. Orange and gray hues with one color backgrounds to maximize the impact of your content image slider for customer logos and working forms.</p>
+                    <ul class="list-unstyled li-space-lg mb-5">
+                        <li class="media">
+                            <i class="fas fa-square"></i>
+                            <div class="media-body"><strong>Card slider</strong> for testimonials, statistics numbers for important values image slider for logos</div>
+                        </li>
+                        <li class="media">
+                            <i class="fas fa-square"></i>
+                            <div class="media-body"><strong>Dropdown navigation</strong> and useful extra pages for article details, terms conditions and privacy policy</div>
+                        </li>
+                        <li class="media">
+                            <i class="fas fa-square"></i>
+                            <div class="media-body"><strong>Clean light design</strong> mixing purple, orange and gray hues with one color backgrounds to maximize</div>
+                        </li>
+                        <li class="media">
+                            <i class="fas fa-square"></i>
+                            <div class="media-body"><strong>Together with</strong> touches of call to action green, the overall landing page design will showcase your app</div>
+                        </li>
+                        <li class="media">
+                            <i class="fas fa-square"></i>
+                            <div class="media-body"><strong>This template</strong> is built with HTML, CSS and Bootstrap to ensure the highest flexibility for all users</div>
+                        </li>
+                    </ul>
+
+                    <a class="btn-solid-reg mb-5" href="index.html">Back</a>
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of ex-basic-1 -->
+    <!-- end of basic -->
