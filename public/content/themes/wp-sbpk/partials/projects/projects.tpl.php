6287b642e589ff51adcd21a8f10ee74da34f928f
Brewalan <sbpk.pro@gmail.com>
2022-03-18 11:57:13 +0100

    Feat minor change

--- public/content/themes/wp-sbpk/partials/projects/projects.tpl.php
+++ public/content/themes/wp-sbpk/partials/projects/projects.tpl.php
@@ -25,16 +25,20 @@ $technologies = get_the_terms(
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                    <?php if ($hasImage) :
-                        $thumbnail = get_the_post_thumbnail_url(); ?>
-                        <a href="<?= $hasWebsiteLink; ?>" target="_blank"><img class="img-fluid img-thumbnail mt-2 mb-3" src="<?= $thumbnail; ?>" alt="picture of Tribut project"></a>
-                    <?php else : ?>
-                        <p>NO DOCUMENT TO DISPLAY</p>
-                    <?php endif; ?>
+                <?php if ($hasImage) :
+                    $thumbnail = get_the_post_thumbnail_url(); ?>
+                    <a href="<?= $hasWebsiteLink; ?>" target="_blank"><img class="img-fluid img-thumbnail mt-2 mb-3" src="<?= $thumbnail; ?>" alt="picture of Tribut project"></a>
+                <?php else : ?>
+                    <p>NO DOCUMENT TO DISPLAY</p>
+                <?php endif; ?>
             </div> <!-- end of col -->
             <div class="projectCenter">
-                <p class="projectCenter">You can click on the link next below to open the website into another windows...</p>
-                <p><a href="<?= $hasWebsiteLink; ?>" target="_blank"><?= get_the_title(); ?>, the website</a></p>
+                <?php if ($hasWebsiteLink) : ?>
+                    <p class="projectCenter">You can click on the link next below to open the website into another windows...</p>
+                    <p><a href="<?= $hasWebsiteLink; ?>" target="_blank"><?= get_the_title(); ?>, the website</a></p>
+                <?php else : ?>
+                    <p>If you want to try it, it will be better to do a git clone...</p>
+                <?php endif; ?>
             </div>
         </div> <!-- end of row -->
     </div> <!-- end of container -->
@@ -49,6 +53,7 @@ $technologies = get_the_terms(
             <div class="col-xl-10 offset-xl-1">
                 <p><?php the_content(); ?></p>
                 <p>You can check the code on <a href="<?= get_field('github') ?>" target="_blank">Github</a></p>
+                <p>First commit in <?= get_field('first_commit') ?></p>
             </div> <!-- end of col -->
         </div> <!-- end of row -->
     </div> <!-- end of container -->
@@ -64,15 +69,15 @@ $technologies = get_the_terms(
                 <h2 class="mb-4">Technologies & tools</h2>
 
                 <ul class="list-unstyled li-space-lg mb-5">
-                    <?php foreach ($technologies as $key =>$value) :
+                    <?php foreach ($technologies as $key => $value) :
                         $technoLink = get_field('technoLink', 'technology_' . $value->term_id); ?>
-                    <li class="media">
-                        <i class="fas fa-square"></i>
-                        <div class="media-body"><strong>
-                                <?= $value->name; ?>
-                            </strong> : if you want to know more about <?= $value->name; ?>, you can check the <a href="<?= $technoLink ?>" target="_blank">official website</a></div>
+                        <li class="media">
+                            <i class="fas fa-square"></i>
+                            <div class="media-body"><strong>
+                                    <?= $value->name; ?>
+                                </strong> : if you want to know more about <?= $value->name; ?>, you can check the <a href="<?= $technoLink ?>" target="_blank">official website</a></div>
                         </li>
-                        <?php endforeach; ?>
+                    <?php endforeach; ?>
                 </ul>
 
             </div> <!-- end of col -->
@@ -82,7 +87,7 @@ $technologies = get_the_terms(
 <!-- end of basic -->
 
 <div class="container">
-<p><?php get_template_part('partials/post-previous-next.tpl'); ?></p>
+    <p><?php get_template_part('partials/post-previous-next.tpl'); ?></p>
 </div>
 
 <!-- The rest of the layout for this page is in the file projects.init.php if needed later -->
